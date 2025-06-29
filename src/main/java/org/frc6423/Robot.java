186a47fac91d68b05fbdb6f011dfcefdf57bf588
Dasun Abeykoon <Dasun20202020@gmail.com>
2025-06-28 21:15:31 -0700

    Feat: Remove RobotContainer class
    Signed-off-by: Dasun Abeykoon <Dasun20202020@gmail.com>

--- src/main/java/org/frc6423/Robot.java
+++ src/main/java/org/frc6423/Robot.java
@@ -4,7 +4,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // MIT license file in the root directory of this project
 
-package frc.robot;
+package org.frc6423;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -13,10 +13,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
-  private final RobotContainer m_robotContainer;
-
   public Robot() {
-    m_robotContainer = new RobotContainer();
   }
 
   @Override
@@ -35,7 +32,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
