aa50c51a95bfe441b662ee149f26408f7c398057
Dasun Abeykoon <Dasun20202020@gmail.com>
2025-06-28 21:45:09 -0700

    feat: create gh workflows for on-push and on-pr
    Signed-off-by: Dasun Abeykoon <Dasun20202020@gmail.com>

--- src/main/java/org/frc6423/robot/Robot.java
+++ src/main/java/org/frc6423/robot/Robot.java
@@ -1,9 +1,3 @@
-// TODO Put Year Copyright (c) {year} FRC 6423 - Ward Melville Iron Patriots
-// https://github.com/FIRSTTeam6423
-// 
-// Open Source Software; you can modify and/or share it under the terms of
-// MIT license file in the root directory of this project
-
 package org.frc6423.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
