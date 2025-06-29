273d3b914e7f969101217d99f851ceb19f80463b
Dasun Abeykoon <Dasun20202020@gmail.com>
2025-06-28 21:50:29 -0700

    feat: create global constants class
    Signed-off-by: Dasun Abeykoon <Dasun20202020@gmail.com>

---
+++ src/main/java/org/frc6423/robot/Constants.java
@@ -0,0 +1,11 @@
+package org.frc6423.robot;
+
+import static edu.wpi.first.units.Units.Seconds;
+
+import edu.wpi.first.units.measure.Time;
+
+public class Constants {
+    public class Flags {
+        public static final Time LOOPTIME = Seconds.of(0.02);
+    }
+}
