8ef1ab891e4b2d3984b07d444864adfb3d63b29b
Dasun Abeykoon <Dasun20202020@gmail.com>
2025-06-28 21:28:47 -0700

    chore: upload filtered team lib
    Signed-off-by: Dasun Abeykoon <Dasun20202020@gmail.com>

--- src/main/java/org/frc6423/robot/Robot.java
+++ src/main/java/org/frc6423/robot/Robot.java
@@ -4,7 +4,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // MIT license file in the root directory of this project
 
-package org.frc6423;
+package org.frc6423.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
